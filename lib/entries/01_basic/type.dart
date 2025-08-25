8ae5907f7c4dc2e9edfa56b4702e13ddf7efbfe1
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-08-26 06:56:01 +0900

    변수 타입

---
+++ lib/entries/01_basic/type.dart
@@ -0,0 +1,18 @@
+void main() {
+  // String 문자열
+  String name = 'test';
+
+  // int 정수
+  int isInt = 10;
+
+  // double 실수
+  double isDouble = 2.5;
+
+  // bool 불리언
+  bool isTrue = true;
+
+  print(name);
+  print(isInt);
+  print(isDouble);
+  print(isTrue);
+}
