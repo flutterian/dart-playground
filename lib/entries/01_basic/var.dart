295ceded0257789eadd20111a103e5a4de3938e1
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-08-26 06:27:11 +0900

    var 를 사용한 변수 선언

---
+++ lib/entries/01_basic/var.dart
@@ -0,0 +1,12 @@
+void main() {
+  var name = 'bm choi';
+  print(name);
+
+  // 변수값 변경 가능
+  name = 'test';
+  print(name);
+
+  // 변수명 중복은 불가능
+  // 그래서 다음 코드에서 주석을 제거하면 에러 발생
+  // var name = '헬로';
+}
