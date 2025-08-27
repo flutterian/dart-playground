1a1254162ff50ac58739291b20ced1b4ff9ebf7b
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-08-28 07:18:27 +0900

    map() 함수

--- lib/entries/01_basic/list.dart
+++ lib/entries/01_basic/list.dart
@@ -10,18 +10,22 @@ void main() {
   blackPinkList[3] = '파스타';
   print(blackPinkList); // [리사, 지수, 제니, 파스타]
 
-  // add() 함수 ~ JS: push() 느낌
+  /** add() 함수 ~ JS: push() 느낌 */
   blackPinkList.add('테스트');
   print(blackPinkList); // [리사, 지수, 제니, 파스타, 테스트]
 
-  // removeLast() 함수 ~ JS: pop() 느낌
+  /** removeLast() 함수 ~ JS: pop() 느낌 */
   String last = blackPinkList.removeLast();
   print(blackPinkList); // [리사, 지수, 제니, 파스타]
   print(last); // 테스트
 
-  // where() 함수 ~ JS: filter() 느낌, 반환 값이 달라지긴함.
+  /** where() 함수 ~ JS: filter() 느낌, 반환 값이 iterable로 달라지긴함. */
   final newList = blackPinkList.where((name) => name == '리사' || name == '지수');
-
   print(newList); // (리사, 지수) - iterable이 반환됨.
   print(newList.toList()); // [리사, 지수]
+
+  /** map() 함수 ~ JS: map(), iterable 반환 */
+  final newBlackPink = blackPinkList.map((name) => '블랙핑크 $name');
+  print(newBlackPink); // (블랙핑크 리사, 블랙핑크 지수, 블랙핑크 제니, 블랙핑크 파스타)
+  print(newBlackPink.toList()); // [블랙핑크 리사, 블랙핑크 지수, 블랙핑크 제니, 블랙핑크 파스타]
 }
