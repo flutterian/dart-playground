424f380949bc7265cbfedfb8c5f0faf336490fe1
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-08-29 06:37:05 +0900

    Map 키와 값 반환 keys, values

--- lib/entries/01_basic/Map.dart
+++ lib/entries/01_basic/Map.dart
@@ -9,4 +9,10 @@ void main() {
   print(
     dictionary,
   ); // {Harry potter: 해리 포터, Ron Weasley: 론 위즐리, Hermione Granger: 헤르미온느 그레인저}
+
+  /** 키와 값 반환하기 */
+  print(
+    dictionary.keys,
+  ); // (Harry potter, Ron Weasley, Hermione Granger) - iterator
+  print(dictionary.values); // (해리 포터, 론 위즐리, 헤르미온느 그레인저) - iterator
 }
