56622df6087580979a7736b3b49637a9cd30b056
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-08-30 09:53:40 +0900

    [typedef] 함수의 매개변수로 함수 넘길 때도 typedef 로 정의해줄수있음

--- lib/entries/01_basic/typedef.dart
+++ lib/entries/01_basic/typedef.dart
@@ -11,12 +11,21 @@ void subtract(int x, int y) {
   print('결과값: ${x - y}');
 }
 
+/// 다트에서 함수는 일급 객체이므로 함수를 값처럼 사용할 수 있다. 자바스크립트랑 똑같음.
+void calculation(int x, int y, Operation oper) {
+  oper(x, y);
+}
+
 void main() {
   /// typedef는 일반적인 변수의 type처럼 사용 가능
   Operation oper = add;
   oper(1, 2); // 3
 
+  calculation(4, 3, oper); // 7
+
   ///subtract() 함수도 Operation에 해당되는 시그니처이므로 oper 변수에 저장가능
   oper = subtract;
   oper(1, 2); // -1
+
+  calculation(4, 3, oper); // 1
 }
