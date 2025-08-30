8242d43bc76b9acd3fe3800776039162f92b1c20
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-08-30 09:10:50 +0900

    [제어문] - switch 문

--- lib/entries/01_basic/control.dart
+++ lib/entries/01_basic/control.dart
@@ -1,6 +1,8 @@
+enum Status { approved, pending, rejected }
+
 void main() {
   /// if 문
-  int number = 4;
+  final int number = 4;
 
   if (number % 3 == 0) {
     print('3의 배수');
@@ -9,4 +11,23 @@ void main() {
   } else {
     print('맞는 조건이 없습니다.');
   }
+
+  /// switch 문
+  final Status status = Status.approved;
+
+  switch (status) {
+    case Status.approved:
+      print('승인 상태');
+      break;
+    case Status.pending:
+      print('대기 상태');
+      break;
+    case Status.rejected:
+      print('거절 상태');
+      break;
+    default:
+      print('알 수 없는 상태');
+  }
+
+  print(Status.values);
 }
